49fadf1838fe543dfe6b5b7a2a36b2e670f79aea
Justin Gery <jaygery90@gmail.com>
2025-08-12 12:38:49 -0500

    removed duplicate cta

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -17,7 +17,7 @@ function LandingPage() {
             <WhyWeDoSection />
             <ScrollingGallery/>
             <LandingInfo />
-            <CTAButton />
+            {/* <CTAButton /> */}
         </div>
     )
 }
